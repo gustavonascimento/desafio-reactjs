0eb4c1bcd3284798c20a4f925d37b4f316d4eefa
Gustavo Moreira <gustavomn93@gmail.com>
2020-04-09 11:04:23 -0300

    Adicionando funções POST e DELETE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })  
+  }, []); 
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Repositorio do Gustavo ${Date.now()}`,
+      url: "https://github.com/gustavonascimento/BlackJack",
+      techs: "Haskell"
+    });
+
+    const repositorie = response.data;
+
+    setRepositories([... repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    const repositorie = response.data;
+
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie => (
+          <li key={repositorie.id}>
+            <ul>
+              {repositorie.title}
+            </ul>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repositorie.id)} type="button">
+              Remover
+            </button>
+         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
